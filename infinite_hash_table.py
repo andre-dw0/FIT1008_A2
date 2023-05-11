3e65a7519be08c3bfc5c50dbac2b917bbed5534e
andre-dw0 <daviswilliamsandre@gmail.com>
2023-05-12 09:51:38 +1000

    Good Progress on Task 4
    The push herein is composed of the following changes:
    - Made a start on the infinite hash table and some decent progress, implemented insert / update functionality and access functionality, just need to figure out how I'm going to add in delete and get location methods.

--- infinite_hash_table.py
+++ infinite_hash_table.py
@@ -6,6 +6,7 @@ from data_structures.referential_array import ArrayR
 K = TypeVar("K")
 V = TypeVar("V")
 
+
 class InfiniteHashTable(Generic[K, V]):
     """
     Infinite Hash Table.
@@ -17,11 +18,11 @@ class InfiniteHashTable(Generic[K, V]):
 
     Unless stated otherwise, all methods have O(1) complexity.
     """
-
     TABLE_SIZE = 27
 
-    def __init__(self) -> None:
-        raise NotImplementedError()
+    def __init__(self):
+        self.level = 0
+        self.array = ArrayR(self.TABLE_SIZE)
 
     def hash(self, key: K) -> int:
         if self.level < len(key):
@@ -34,13 +35,33 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
+        position = self.hash(key)
+        if self.array[position] is None:
+            raise KeyError(key)
+        else:
+            if self.array[position].isinstance(InfiniteHashTable):
+                return self.array[position][key]
+            else:
+                return self.array[position]
 
     def __setitem__(self, key: K, value: V) -> None:
         """
         Set an (key, value) pair in our hash table.
         """
-        raise NotImplementedError()
+        position = self.hash(key)
+        if self.array[position] is not None:
+            # Need to account for none first
+            if isinstance(self.array[position][1], InfiniteHashTable):
+                self.array[position][1][key] = value
+            else:
+                new_table = InfiniteHashTable()
+                new_table.level = self.level + 1
+                old_key, old_value = self.array[position]
+                self.array[position] = (key[new_table.level-1], new_table)
+                self.array[position][1][key] = (key, value)
+                self.array[position][1][old_key] = (old_key, old_value)
+        else:
+            self.array[position] = (key, value)
 
     def __delitem__(self, key: K) -> None:
         """
@@ -59,7 +80,7 @@ class InfiniteHashTable(Generic[K, V]):
 
         Not required but may be a good testing tool.
         """
-        raise NotImplementedError()
+        return str(self.table)
 
     def get_location(self, key):
         """
