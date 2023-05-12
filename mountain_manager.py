4dcc15a0f15e20320581b3e14439e6217bc49a44
andre-dw0 <daviswilliamsandre@gmail.com>
2023-05-12 20:34:51 +1000

    Big Updates to Task 6
    NO longer does task 6 contain incorrect implementations such as relying on a list, not sorting elements, and so on, in this push we do the following:
    - Use a double keyed hash table instead of a list.
    - Utilize merge sort to sort by difficulty level and name, such that group_by_difficulty works as per the task 6 specifications.

--- mountain_manager.py
+++ mountain_manager.py
@@ -1,25 +1,34 @@
+from double_key_table import DoubleKeyTable
 from mountain import Mountain
+from algorithms.mergesort import merge, mergesort
 
 
 class MountainManager:
 
     def __init__(self) -> None:
-        self.mountains = []
+        self.mountain_table = DoubleKeyTable()
 
-    def add_mountain(self, mountain: Mountain):
-        if mountain in self.mountains:
-            self.mountains.append(mountain)
-        else:
-            raise KeyError(Mountain)
+    def add_mountain(self, mountain: Mountain) -> None:
+        self.mountain_table[str(mountain.difficulty_level),
+                            mountain.name] = mountain
 
-    def remove_mountain(self, mountain: Mountain):
-        self.mountains.remove(mountain)
+    def remove_mountain(self, mountain: Mountain) -> None:
+        del self.mountain_table[mountain.difficulty_level, mountain.name]
 
     def edit_mountain(self, old: Mountain, new: Mountain):
-        raise NotImplementedError()
+        self.remove_mountain(old)
+        self.add_mountain(new)
 
     def mountains_with_difficulty(self, diff: int):
-        raise NotImplementedError()
+        return self.mountain_table.keys(str(diff))
 
     def group_by_difficulty(self):
-        raise NotImplementedError()
+        mountain_lists = []
+        for difficulty_level in self.keys():
+            mountain_list = self.mountain_table.keys(
+                difficulty_level)
+            mountain_list = mergesort(mountain_list, key=lambda m: m.name)
+            mountain_lists.append(mountain_list)
+        mountain_lists = mergesort(
+            mountain_lists, key=lambda lst: lst[0].difficulty)
+        return mountain_lists
